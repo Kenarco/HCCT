1ffa08c012d08a5d334c9a396f6c1060c20c2f3f
Ken Stevens <kensarco120353@gmail.com>
2023-11-02 13:10:17 +0800

    Fix typo in about page"

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,20 @@
+
+const moment = require('moment');
+
+moment.locale('en');
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/");
     eleventyConfig.addPassthroughCopy("src/css/");
 
     eleventyConfig.addWatchTarget("/src/css");
+    eleventyConfig.addFilter('dateIso', date => {
+	return moment(date).toISOString();
+    });
+    
+    eleventyConfig.addFilter('dateReadable', date => {
+	return moment(date).utc().format('LL'); // E.g. May 31, 2019
+    });
 
     return {
         templateFormats: [
